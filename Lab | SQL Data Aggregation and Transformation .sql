b5f5db85970aca98bb2ea868e5f133370c681b45
astrid275 <astrid275@gmail.com>
2025-07-24 08:12:08 +0200

    Solved Lab
    Challenge 1, Questions 1-3
    Without bonus 2.3, 4.
    Without Challenge 2, questions 1-3.

---
+++ Lab | SQL Data Aggregation and Transformation .sql
@@ -0,0 +1,9 @@
+
+
+
+
+# 1:  You need to use SQL built-in functions to gain insights relating to the duration of movies:
+
+1.1 Determine the shortest and longest movie durations and name the values as max_duration and min_duration.
+1.2. Express the average movie duration in hours and minutes. Don't use decimals.
+Hint: Look for floor and round functions.
